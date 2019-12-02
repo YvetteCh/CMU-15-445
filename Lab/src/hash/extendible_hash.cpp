84040843695e5e89515a9e7205977b0932b18910
YvetteCh <yettech@gmail.com>
2019-12-02 18:27:58 +0800

    finish_hash_for_test

--- Lab/src/hash/extendible_hash.cpp
+++ Lab/src/hash/extendible_hash.cpp
@@ -135,143 +135,74 @@ void ExtendibleHash<K, V>::Insert(const K &key, const V &value) {
     }
 
     bucket_[position] -> items[key] = value;
-    
-    // global_depth = bucket_depth, split, regular
-    if(depth = bucket_[position]->depth) {
+    pair_count_++;
 
-    } else {
-        bucket_[position] = std::make_shared<Bucket>(position, )
+    auto bucket = bucket_[position];
+    if(bucket->items.size() > bucket_size_) {
+        auto old_index = bucket->id;
+        auto new_bucket = split(bucket);
+        auto old_size = bucket_.size();
+        auto old_depth = depth;
+        
+        if(new_bucket->depth > depth) {    
+            auto factor = 1<<(new_bucket-> depth - old_depth);
+            bucket_.resize(old_size * factor);
+            bucket_[bucket->id] = bucket;
+            bucket_[new_bucket->id] = new_bucket->id;
+
+            for (int i = 0; i < bucket_.size(); i++) {
+                if(bucket_[i]) {
+                    auto step = 1 << bucket_[i]->depth;
+                    for(int j = i + step; j < bucket_.size(); j+= step)
+                        bucket_[j] = bucket_[i];
+                }
+            }
+        } else {
+            for(int i = old_depth; i < bucket_.size(); i++) {
+                bucket_[i].reset();
+            }
+            bucket_[bucket->id] = bucket;
+            bucket_[new_bucket->id] = new_bucket;
+            auto step = 1 << bucket->depth;
+            for(int i = bucket->id + step; i < bucket_.size(); i+= step)
+                bucket_[i] = bucket_[bucket];
+            for(int i = new_bucket->id + step; i < bucket_.size(); i+= step)
+                bucket_[i] = bucket_[new_bucket];
+        }
     }
-
-    // global_depth > bucket_depth, no split, regular
 
 }
 
 
 template<typename K, typename V>
 std::shared_ptr<typename ExtendibleHash<K, V>::Bucket> 
-ExtendibleHash<K, V>::split(std::shared_ptr<Bucket> &) {
-
-}
-
-
-
-
-
-
-template <typename K, typename V>
-void ExtendibleHash<K, V>::Insert(const K &key, const V &value) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    size_t bucket_id = HashKey(key) & ((1 << depth) - 1);
-
-    // 找到插入的位置，如果为空则新建一个桶
-    if(bucket_[bucket_id] == nullptr) {
-        bucket_[bucket_id] = std::make_shared<Bucket>(bucket_id, depth);
-        ++bucket_count_;
-    }
-    auto bucket = bucket_[bucket_id];
-
-    // 如果该位置有值，则覆盖
-    if(bucket->items.find(key) != bucket->items.end()) {
-        bucket->items[key] = value;
-        return;
-    }
-
-    // 插入键值对
-    bucket->items.insert({key, value});
-    ++pair_count_;
-
-    // 需要分裂桶以及重新分配
-    if(bucket->items.size() > bucket_size_) {
-        // 先记录旧的下标和全局深度
-        auto old_index = bucket->id;
-        auto old_depth = bucket->depth;
-
-        std::shared_ptr<Bucket> new_bucket = split(bucket);
-
-        // 溢出了就改回原来的深度
-        if(new_bucket == nullptr) {
-            bucket->depth = old_depth;
-            return;
-        }
-
-        // 若插入的桶的局部深度大于全局深度，则要扩展桶数组
-        if(bucket->depth > depth) {
-            auto size = bucket_.size();
-            auto factor = (1 << (bucket->depth - depth));
-
-            depth = bucket->depth;
-            bucket_.resize(bucket_.size()*factor);
-
-            // 修改和添加要插入的桶和新建的桶
-            bucket_[bucket->id] = bucket;
-            bucket_[new_bucket->id] = new_bucket;
-
-            // 遍历桶数组
-            for(size_t i = 0; i < size; ++i) {
-                if(bucket_[i]) {
-                    if(i < bucket_[i]->id){
-                        bucket_[i].reset();
-                    } else {
-                        auto step = 1 << bucket_[i]->depth;
-                        for(size_t j = i + step; j < bucket_.size(); j += step) {
-                            bucket_[j] = bucket_[i];
-                        }
-                    }
-                }
-            }
-        } else {
-            for (size_t i = old_index; i < bucket_.size(); i += (1 << old_depth)) {
-                bucket_[i].reset();
-            }
-
-            bucket_[bucket->id] = bucket;
-            bucket_[new_bucket->id] = new_bucket;
-
-            auto step = 1 << bucket->depth;
-            for (size_t i = bucket->id + step; i < bucket_.size(); i += step) {
-                bucket_[i] = bucket;
-            }
-            for (size_t i = new_bucket->id + step; i < bucket_.size(); i += step) {
-                bucket_[i] = new_bucket;
-            }
-        }
-    }
-}
-
-// 分裂新桶
-template <typename K, typename V>
-std::shared_ptr<typename ExtendibleHash<K, V>::Bucket>
 ExtendibleHash<K, V>::split(std::shared_ptr<Bucket> &b) {
-    // 先创建一个新桶
-    auto res = std::make_shared<Bucket>(0, b->depth);
-    // 注意：这里是while循环
-    while(res->items.empty()) {
-        // 先将深度加一
+    auto new_bucket = std::make_shared<Bucket>(0, b->depth);
+    while(new_bucket->items.empty()) {
+        new_bucket->depth++;
         b->depth++;
-        res->depth++;
-        // 下面的for循环实现两个桶的分配
         for(auto it = b->items.begin(); it != b->items.end();) {
-            // 注意下面两个HashKey与后面的式子是不一样的
-            if (HashKey(it->first) & (1 << (b->depth - 1))) {
-                res->items.insert(*it);
-                res->id = HashKey(it->first) & ((1 << b->depth) - 1);
+            if(HashKey(it->first) & (1 << (b->depth - 1))) {
+                new_bucket->items.insert({it->first, it->second});
+                new_bucket -> id = HashKey(it->first) & ((1 << b->depth) - 1);
                 it = b->items.erase(it);
             } else {
-                ++it;
+                it++;
             }
         }
 
-        // 如果b桶的map为空，说明深度不够，还要进行循环
         if(b->items.empty()) {
-            b->items.swap(res->items);
-            b->id = res->id;
+            swap(b->items, new_bucket->items);
+            b->id = new_bucket->id;
         }
     }
 
-    ++bucket_count_;
-    return res;
+    bucket_count_++;
+    return new_bucket;
 }
+
+
+
 
 template class ExtendibleHash<page_id_t, Page *>;
 template class ExtendibleHash<Page *, std::list<Page *>::iterator>;
